9c0644f8d8358f58eb73492bd70c9f6529745970
hexblade12 <78049247+hexblade12@users.noreply.github.com>
2021-03-15 13:18:52 -0400

    Randomize demo

---
+++ Assets/Scripts/ArrayScript.cs
@@ -0,0 +1,24 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayScript : MonoBehaviour
+{
+    public GameObject[] myArrayObjects = new GameObject[1];
+    void Start()
+    {
+       
+
+        int randomFood = Random.Range(0, 2);
+
+        Instantiate(myArrayObjects[randomFood], this.transform.position, Quaternion.identity);
+
+        Debug.Log("The name of the object is:" + randomFood);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
